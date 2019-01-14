a257d878b7d3b721dbfeab00f42bbdc11f3bfc9b
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-01-14 17:55:29 +0100

    Add switch in function slotMachine_randomize to assign pictures to random numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,13 +8,154 @@ function slotMachine_randomize() {
     // integer random number between 0 and <1 multiplied by 10
     // result is output on the HTML page in the element with the ID column1 respectively column2 respectively column3
 
+
+    // column1
+
     column1 = Math.floor(Math.random() * 10);
+
+    switch (column1) {
+
+        case 0:
+            column1 = `<img src="assets/img/dollar.png">`;
+            break;
+
+        case 1:
+            column1 = `<img src="assets/img/coconut.png">`;
+            break;
+
+        case 2:
+            column1 = `<img src="assets/img/bananas.png">`;
+            break;
+
+        case 3:
+            column1 = `<img src="assets/img/kiwi.png">`;
+            break;
+
+        case 4:
+            column1 = `<img src="assets/img/watermelon.png">`;
+            break;
+
+        case 5:
+            column1 = `<img src="assets/img/cherry.png">`;
+            break;
+
+        case 6:
+            column1 = `<img src="assets/img/orange.png">`;
+            break;
+
+        case 7:
+            column1 = `<img src="assets/img/tomato.png">`;
+            break;
+
+        case 8:
+            column1 = `<img src="assets/img/apple.png">`;
+            break;
+
+        case 9:
+            column1 = `<img src="assets/img/strawberry.png">`;
+            break;
+    }
+
     document.getElementById("column1").innerHTML = column1
 
+
+    // column2
+
     column2 = Math.floor(Math.random() * 10);
+
+    switch (column2) {
+
+        case 0:
+            column2 = `<img src="assets/img/dollar.png">`;
+            break;
+
+        case 1:
+            column2 = `<img src="assets/img/coconut.png">`;
+            break;
+
+        case 2:
+            column2 = `<img src="assets/img/bananas.png">`;
+            break;
+
+        case 3:
+            column2 = `<img src="assets/img/kiwi.png">`;
+            break;
+
+        case 4:
+            column2 = `<img src="assets/img/watermelon.png">`;
+            break;
+
+        case 5:
+            column2 = `<img src="assets/img/cherry.png">`;
+            break;
+
+        case 6:
+            column2 = `<img src="assets/img/orange.png">`;
+            break;
+
+        case 7:
+            column2 = `<img src="assets/img/tomato.png">`;
+            break;
+
+        case 8:
+            column2 = `<img src="assets/img/apple.png">`;
+            break;
+
+        case 9:
+            column2 = `<img src="assets/img/strawberry.png">`;
+            break;
+    }
+
     document.getElementById("column2").innerHTML = column2
 
+
+    // column3
+
     column3 = Math.floor(Math.random() * 10);
+
+    switch (column3) {
+
+        case 0:
+            column3 = `<img src="assets/img/dollar.png">`;
+            break;
+
+        case 1:
+            column3 = `<img src="assets/img/coconut.png">`;
+            break;
+
+        case 2:
+            column3 = `<img src="assets/img/bananas.png">`;
+            break;
+
+        case 3:
+            column3 = `<img src="assets/img/kiwi.png">`;
+            break;
+
+        case 4:
+            column3 = `<img src="assets/img/watermelon.png">`;
+            break;
+
+        case 5:
+            column3 = `<img src="assets/img/cherry.png">`;
+            break;
+
+        case 6:
+            column3 = `<img src="assets/img/orange.png">`;
+            break;
+
+        case 7:
+            column3 = `<img src="assets/img/tomato.png">`;
+            break;
+
+        case 8:
+            column3 = `<img src="assets/img/apple.png">`;
+            break;
+
+        case 9:
+            column3 = `<img src="assets/img/strawberry.png">`;
+            break;
+    }
+
     document.getElementById("column3").innerHTML = column3
 
 }
@@ -68,7 +209,7 @@ function slotMachine_round() {
 
                             let profit = parseInt(document.getElementById("bet").value) * 5 // probability 1/10
                             document.getElementById("information").innerHTML = `You have won ${profit} coins!`
-                            
+
                         } else {
                             document.getElementById("column1").style.backgroundColor = "red"
                             document.getElementById("column2").style.backgroundColor = "red"
