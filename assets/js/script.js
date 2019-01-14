0af2aa674588d20f98ccbf4a908f515c23300324
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-01-14 12:23:26 +0100

    Add function slotMaschine_reset and eventListener for it

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,17 @@
 function slotMachine_randomize() {
-    
+
     // integer random number between 0 and <1 multiplied by 10
     // result is output on the HTML page in the element with the ID column1 respectively column2 respectively column3
-    
+
     column1 = Math.floor(Math.random() * 10);
     document.getElementById("column1").innerHTML = column1
-    
+
     column2 = Math.floor(Math.random() * 10);
     document.getElementById("column2").innerHTML = column2
-    
+
     column3 = Math.floor(Math.random() * 10);
     document.getElementById("column3").innerHTML = column3
-    
+
 }
 
 
@@ -19,9 +19,9 @@ function slotMachine_randomize() {
 let i = 1;
 
 function slotMachine_round() {
-    
+
     // 19 numbers will be displayed; display duration of each number
-    
+
     setTimeout(function () {
         slotMachine_randomize();
         i++;
@@ -33,6 +33,14 @@ function slotMachine_round() {
 
 
 
+function slotMachine_reset() {
+    
+    // to play it several times (reset i from 19 to 1)
+    i = 1;
+}
+
+
 // buttons on HTML page
 
 document.querySelector('#play-button').addEventListener('click', slotMachine_round);
+document.querySelector('#play-button').addEventListener('click', slotMachine_reset);
